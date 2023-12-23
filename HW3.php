02a059157bf12aa95d4eae7e46a8366601385653
65160349 <65160349@go.buu.ac.th>
2023-12-24 05:54:13 +0700

    Update HW3.php

--- HW3.php
+++ HW3.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -6,26 +7,53 @@
 
 <body>
     <?php $multi_x = 2; ?>
-    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+    <h1 style="text-align: center;">ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
 
-    <table>
-        <tbody>
-            <tr>
-                <td></td>
-                <td>x</td>
-                <td></td>
-                <td>=</td>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
+    <div style="width: 200px; height: 265px; overflow: auto; margin: 0 auto; text-align: center; font-size: 20px">
+        <table style="margin: auto;">
+            <tbody>
+                <?php for ($i = 1; $i <= 24; $i++) : ?>
+                    <tr>
+                        <td><?php echo $multi_x ?></td>
+                        <td>x</td>
+                        <td><?php echo $i ?></td>
+                        <td>=</td>
+                        <td><?php echo $multi_x * $i ?></td>
+                    </tr>
+                <?php endfor; ?>
+            </tbody>
+        </table>
+    </div>
+
     <style>
+        h1{
+            padding: 20px;
+            text-shadow: 2px 3px 6px rgba(185, 167, 167, 1);
+        }
+        body{
+            background-color: #ffffff;
+            background-image: url('https://a-static.besthdwallpaper.com/beautiful-mountain-nature-wallpaper-1280x720-80752_45.jpg');
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+            min-height: 100vh;
+            
+            
+            
+        }
         table {
-            width: 150px;
-            height: 265px;
+            width: 100%;
+            border-collapse: collapse;
+            backdrop-filter: blur(7px);
+            background-color: rgba(255, 255, 255, 0.4);;
+            
         }
 
-
+        td {
+            text-align: center;
+            padding: 5px;
+            border: 0px solid #ccc;
+        }
     </style>
 </body>
 
